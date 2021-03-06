e9111434121e0110bcf719c9ae2739e51c2fafcb
Diego Fernandes <diego.fernandes721@gmail.com>
2021-03-06 14:50:50 -0300

    PROJETO BÁSICO FINALIZADO

---
+++ CursoMVC/Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Categoria> categorias { get; set; }
+        public DbSet<Produto> produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security = True");
+        }
+    }
+}
